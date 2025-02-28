f4448b6e794901918c899d3a33fced14fdb779e7
Anees <anees774883@gmail.com>
2025-02-28 14:26:52 +0530

    Student Data fetch from central and save into database

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,27 @@
-<<<<<<< HEAD
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
 
-const superAdminRoutes = require('./routes/superAdminRoutes');
+// const superAdminRoutes = require('./routes/superAdminRoutes');
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
 
-app.use('/api/superadmin', superAdminRoutes);
+// app.use('/api/superadmin', superAdminRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-=======
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 require("dotenv").config();
 const express = require("express");
 const authRoutes = require("./routes/auth");
@@ -32,16 +30,26 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 const protectedRoutes = require("./routes/protectedRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const { connectMongoDB } = require("./config/db");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
+connectMongoDB();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/protected", protectedRoutes);
+app.use("/api/students", studentRoutes);
+//Routes
+app.use("/api/students", require("./routes/studentRoutes"));
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
   .catch((err) => console.log("MongoDB Connection Error:", err));
   const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -59,6 +67,3 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use(express.json()); // JSON Parsing
 app.use("/api/users", userRoutes); // Use Routes
-
-
->>>>>>> 5e32d87dd5a15d759e2a1b4ad351244cccfb0b70
