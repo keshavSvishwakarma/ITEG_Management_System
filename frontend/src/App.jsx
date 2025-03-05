fe3e589a1b71d65f04a874c56d18d55675062f81
TanushreeSoni2021 <tanushrees.bca2021@ssism.org>
2025-03-05 15:56:25 +0530

    changes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/deshboard/Dashboard";
 import AdmissionDashboard from "./components/addmitionProcess/AdmissionDashboard";
-import { LogIn } from "lucide-react";
-// import Sidebar from "./components/sidebar/Sidebar";
+// import { LogIn } from "lucide-react";
 
-const Admission = () => (
-  <h2 className="text-2xl p-4">Admission Process Page</h2>
-);
 const StudentRecord = () => (
   <h2 className="text-2xl p-4">Student Record Page</h2>
 );
@@ -22,9 +18,9 @@ function App() {
         {/* <Sidebar role="teacher" /> */}
         <div className="flex-1">
           <Routes>
-          {/* <Route path="/" element={<LogIn />} /> */}
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/admission" element={<AdmissionDashboard />} />
+            {/* <Route path="/" element={<LogIn />} /> */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/admission" element={<AdmissionDashboard />} />
             <Route path="/student-record" element={<StudentRecord />} />
             <Route path="/placement" element={<Placement />} />
           </Routes>
