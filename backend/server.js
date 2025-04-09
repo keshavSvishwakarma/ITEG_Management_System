d7a74f83088c44258f640cb3cc179ca7b69371e2
Azhar-ali-dev <aazahar025@gmail.com>
2025-04-09 15:08:21 +0530

    cors added for frontend and backend communication

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,20 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-
-// const superAdminRoutes = require('./routes/superAdminRoutes');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
-
-// app.use('/api/superadmin', superAdminRoutes);
-
-// const PORT = process.env.PORT || 5000;
-//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+
+// Import Routes
 const adminRoutes = require("./routes/AdminRoutes");
 const facultyRoutes = require("./routes/facultyRoutes");
 const studentAdmissionRoutes = require('./routes/student_admissionProcessRoutes');
 const protectedRoutes = require("./routes/protectedRoutes");
-
+const superAdminRoutes = require("./routes/SuperAdminRoutes")
+//expres object 
 const app = express();
+// cors for frontend and backend communication
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
@@ -46,16 +25,11 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/faculty", facultyRoutes);
 app.use("/api/protected", protectedRoutes);
 app.use('/api/students', studentAdmissionRoutes);
+app.use("/api/superAdmin",superAdminRoutes)
 
 // MongoDB Connection
 mongoose
-  // .connect(process.env.MONGO_URI, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true
-  // })
-  // .then(() => console.log("Connected to MongoDB"))
-  // .catch((err) => console.log("DB Connection Error: ", err));/
-  .connect(process.env.MONGO_URI)
+.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ DB Connection Error:", err));
 
