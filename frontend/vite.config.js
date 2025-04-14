beb4f6fa1f30f741d3a73673f74b90bb6d82f5c4
Bharti6266 <bhartiverma6266@gmail.com>
2025-04-14 13:39:30 +0530

    UnitTestingSetup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// // export default {
+// //   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+// //   testEnvironment: "jsdom",
+// //   moduleNameMapper: {
+// //     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+// //     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+// //   },
+// // };
+
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// ✅ Import vitest config
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js' // create this file next
+  }
+});
