9a6f77ceac080e8296f284cb4314bcac3ad18d2d
Azhar-ali-dev <aazahar025@gmail.com>
2025-04-15 15:55:25 +0530

    webhook router updated

--- backend/routes/webhookRoutes.js
+++ backend/routes/webhookRoutes.js
@@ -5,31 +5,6 @@ const admissionController = require('../modules/student/controllers/admissionPro
 
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-// router.post("/receive/data", (req, res) => {
-//   const receivedSignature = req.headers["x-webhook-signature"];
-//   const payload = req.body;
-
-//   const expectedSignature = crypto
-//     .createHmac("sha256", WEBHOOK_SECRET)
-//     .update(JSON.stringify(payload))
-//     .digest("hex");
-
-//   if (receivedSignature !== expectedSignature) {
-//     console.log("❌ Invalid Signature");
-//     return res.status(401).send("Unauthorized");
-//   }
-
-//   console.log("✅ Webhook received in actual project:");
-
-//   console.log(payload);
-
-//   // 👉 Process the data: Save to DB, update status, etc.
-
-//   res.status(200).send("Webhook received");
-// }
-// );
-
-
 
 router.post("/receive/data",admissionController.addAdmission);
 
