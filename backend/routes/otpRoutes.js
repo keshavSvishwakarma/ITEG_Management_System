8ffc42f4539790d3a8b1513433145ec633a8f491
Azhar-ali-dev <aazahar025@gmail.com>
2025-04-28 15:07:56 +0530

    updated code

--- backend/routes/otpRoutes.js
+++ backend/routes/otpRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { sendOtpToEmail, verifyEmailOtp } = require('D:/GitHub/ITEG_Management_System/backend/modules/user/controllers/otpController.js');
+const { sendOtpToEmail, verifyEmailOtp } = require('../modules/user/controllers/otpController.js');
 
 router.post('/send-otp-email', sendOtpToEmail);
 router.post('/verify-otp-email', verifyEmailOtp);
