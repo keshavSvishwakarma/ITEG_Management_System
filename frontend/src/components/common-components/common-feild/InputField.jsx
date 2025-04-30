dacc6c1af274096110db880f89f85bc49fbec0df
Bharti6266 <123243253+Bharti6266@users.noreply.github.com>
2025-04-30 12:08:34 +0530

    CommonInputField

---
+++ frontend/src/components/common-components/common-feild/InputField.jsx
@@ -0,0 +1,16 @@
+import { useField } from "formik";
+
+const InputField = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="flex flex-col">
+      {label && <label className="font-medium">{label}</label>}
+      <input {...field} {...props} className="input" />
+      {meta.touched && meta.error && (
+        <span className="text-red-500 text-sm">{meta.error}</span>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
