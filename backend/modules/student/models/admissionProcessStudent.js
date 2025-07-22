81129047819aca611d68c012674b5b5bbe177c5c
Anees <anees774883@gmail.com>
2025-07-22 17:11:33 +0530

    now aadhar unique is implement

--- backend/modules/student/models/admissionProcessStudent.js
+++ backend/modules/student/models/admissionProcessStudent.js
@@ -11,7 +11,7 @@ const StudentAdmissionProcessSchema = new mongoose.Schema(
     parentMobile: { type: String, required: true },
     gender: { type: String},
     dob: { type: Date },
-    aadharCard: { type: String},
+    aadharCard: { type: String, unique: true },
     address: { type: String, required: true },
     track: { type: String },   
     village: { type: String, required: true },
